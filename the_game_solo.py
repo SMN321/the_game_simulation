7a07630eef4c4f35e756d286cb7449b3b70d356f
Team 21, Simon <simon.wilhelmstaetter@uni-ulm.de>
2020-09-27 21:02:12 +0200

    added multi-run

--- the_game_solo.py
+++ the_game_solo.py
@@ -1,44 +1,56 @@
-
+# todo: minimize stacks to single ints (no need to save the whole stack)
 import random
 
 HANDSIZE = 7
 MINCOUNT = 3
 MAXNUM = 100
+RUNCOUNT = 1000
 
-shuffled = list(range(1, MAXNUM))
-random.shuffle(shuffled)
+results = ([], [])  # for len(shuffled) and len(hand)
 
-stacks = ([0], [MAXNUM], [0], [MAXNUM])
+for run in range(RUNCOUNT):
+    shuffled = list(range(1, MAXNUM))
+    random.shuffle(shuffled)
 
-hand = []
+    stacks = ([0], [MAXNUM], [0], [MAXNUM])
 
-count = MINCOUNT
-while count == MINCOUNT:
-    # no more cards left
-    if len(shuffled) == 0 and len(hand) == 0:
-        break
+    hand = []
 
-    # fill the hand
-    while len(hand) < HANDSIZE and len(shuffled) > 0:
-        hand.append(shuffled.pop())
-
-    # play MINCOUNT cards
-    count = 0
-    for i in range(MINCOUNT):
-        min_dist = MAXNUM
-        stack_num = -1
-        card_num = 0
-        # search for the card with the minimal distance to one of the stacks
-        for card, index in zip(hand, range(len(hand))):
-            for i in range(len(stacks)):
-                diff = card - stacks[i][-1] if i % 2 == 0 else stacks[i][-1] - card
-                if diff == -10: min_dist = 0; stack_num = i; card_num = index  # 10-jump rule
-                if min_dist > diff > 0: min_dist = diff; stack_num = i; card_num = index 
-
-        if stack_num != -1:
-            stacks[stack_num].append(hand.pop(card_num))
-            count += 1
-        else:
+    count = MINCOUNT
+    while count == MINCOUNT:
+        # no more cards left
+        if len(shuffled) == 0 and len(hand) == 0:
             break
 
-print("Länge shuffled: %d, Länge hand: %d" % (len(shuffled), len(hand)))
+        # fill the hand
+        while len(hand) < HANDSIZE and len(shuffled) > 0:
+            hand.append(shuffled.pop())
+
+        # play MINCOUNT cards
+        count = 0
+        for i in range(MINCOUNT):
+            min_dist = MAXNUM
+            stack_num = -1
+            card_num = 0
+            # search for the card with the minimal distance to one of the stacks
+            for card, index in zip(hand, range(len(hand))):
+                for i in range(len(stacks)):
+                    diff = card - stacks[i][-1] if i % 2 == 0 else stacks[i][-1] - card
+                    if diff == -10: min_dist = 0; stack_num = i; card_num = index  # 10-jump rule
+                    if min_dist > diff > 0: min_dist = diff; stack_num = i; card_num = index 
+
+            if stack_num != -1:
+                stacks[stack_num].append(hand.pop(card_num))
+                count += 1
+            else:
+                break
+
+    results[0].append(len(shuffled))
+    results[1].append(len(hand))
+
+avg_shuffled = sum(results[0]) / len(results[0])
+avg_hand = sum(results[1]) / len(results[1])
+
+print("Länge shuffled: %f, Länge hand: %f" % (avg_shuffled, avg_hand))
+
+
