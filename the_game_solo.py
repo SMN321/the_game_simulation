c946c6a2bb619eb1697bda6cb5333a73523425b2
Team 21, Simon <simon.wilhelmstaetter@uni-ulm.de>
2020-09-27 20:45:19 +0200

    added the 10-jump rule

--- the_game_solo.py
+++ the_game_solo.py
@@ -8,7 +8,7 @@ MAXNUM = 100
 shuffled = list(range(1, MAXNUM))
 random.shuffle(shuffled)
 
-stacks = ([0], [0], [MAXNUM], [MAXNUM])
+stacks = ([0], [MAXNUM], [0], [MAXNUM])
 
 hand = []
 
@@ -30,11 +30,11 @@ while count == MINCOUNT:
         card_num = 0
         # search for the card with the minimal distance to one of the stacks
         for card, index in zip(hand, range(len(hand))):
-            if min_dist > card - stacks[0][-1] > 0: min_dist = card - stacks[0][-1]; stack_num = 0; card_num = index 
-            if min_dist > card - stacks[1][-1] > 0: min_dist = card - stacks[1][-1]; stack_num = 1; card_num = index
-            if min_dist > stacks[2][-1] - card > 0: min_dist = stacks[2][-1] - card; stack_num = 2; card_num = index 
-            if min_dist > stacks[3][-1] - card > 0: min_dist = stacks[3][-1] - card; stack_num = 3; card_num = index 
-        
+            for i in range(len(stacks)):
+                diff = card - stacks[i][-1] if i % 2 == 0 else stacks[i][-1] - card
+                if diff == -10: min_dist = 0; stack_num = i; card_num = index  # 10-jump rule
+                if min_dist > diff > 0: min_dist = diff; stack_num = i; card_num = index 
+
         if stack_num != -1:
             stacks[stack_num].append(hand.pop(card_num))
             count += 1
